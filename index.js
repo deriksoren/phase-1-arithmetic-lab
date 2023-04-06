7c8e597d97d8bb539c14c532d703ad22e47b585b
Erik Sorensen <deriksoren@gmail.com>
2023-04-06 10:41:09 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1=124
+let num2= 0.5
+const multiply = num1*num2;
+const random = Number.isInteger(random) +1;
+const num3=24;
+const num4=5
+const mod = num3 % num4;
+const max = Math.max (0,20,18,4);
